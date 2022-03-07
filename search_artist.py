294d9604a4f4e354086fc612bc10d3e34ac7fd82
km0m <99661522+km0m@users.noreply.github.com>
2022-03-07 01:47:37 +0100

    Added initial file search_artist.py

---
+++ search_artist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 18:42:34 2022
+
+@author: Koala
+"""
+
+import musicbrainzngs
+import sys
+
+musicbrainzngs.set_useragent(
+    "python-search-artist_example",
+    "0.1",
+    "https://github.com/km0m/Manage-song-files.git",
+)
+
+musicbrainzngs.get_artist_by_id('Oasis', includes=['recordings', 'releases', 'discids', 'media'])
