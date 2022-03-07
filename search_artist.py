c509e9aa9bad40b0b48571118558dc3d9dbbdbc5
km0m <99661522+km0m@users.noreply.github.com>
2022-03-07 19:21:08 +0100

    Added authentification

--- search_artist.py
+++ search_artist.py
@@ -6,22 +6,28 @@ Created on Sun Mar  6 18:42:34 2022
 """
 
 import musicbrainzngs
+import getpass
 import sys
 
-agent_name = "password_test.py"
+agent_name = "search-artist.py"
 agent_version = "0.1"
 # this script doesn't submit anything (empty dict)
 # so testing this on live server (with live passwords) is okay.
 musicbrainz_server = "test.musicbrainz.org"
 #musicbrainz_server = "musicbrainz.org"
 
+musicbrainzngs.set_hostname(musicbrainz_server)
 musicbrainzngs.set_useragent(
-    "python-search-artist_example",
-    "0.1",
+    agent_name,
+    agent_version,
     "https://github.com/km0m/Manage-song-files.git",
 )
+sys.stdout.write("Please input your Musicbrainz username: ")
+username = user_input()
+password = getpass.getpass("Enter your Musicbrainz password: ")
 
-musicbrainzngs.set_rate_limit(limit_or_interval=1.0, new_requests=1)
+musicbrainzngs.auth(username, password)
+musicbrainzngs.set_rate_limit(limit_or_interval=False, new_requests=1)
 
 musicbrainzngs.musicbrainz.VALID_RELEASE_STATUSES = ['official']
 
