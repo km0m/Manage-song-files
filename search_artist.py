607d71bb2c7a2c7e896a707b0138474b36bf8211
Km0m <karolina.ordonez@gmail.com>
2022-03-07 20:09:25 +0100

    Changed host server & removed authentification Minor adjustment to script and changed call to search_artist

--- search_artist.py
+++ search_artist.py
@@ -8,13 +8,15 @@ Created on Sun Mar  6 18:42:34 2022
 import musicbrainzngs
 import getpass
 import sys
+from urllib.request import Request, urlopen
+
+artist_name = input("Enter artist name: ")
 
 agent_name = "search-artist.py"
 agent_version = "0.1"
 # this script doesn't submit anything (empty dict)
 # so testing this on live server (with live passwords) is okay.
-musicbrainz_server = "test.musicbrainz.org"
-#musicbrainz_server = "musicbrainz.org"
+musicbrainz_server = "musicbrainz.org"
 
 musicbrainzngs.set_hostname(musicbrainz_server)
 musicbrainzngs.set_useragent(
@@ -22,13 +24,10 @@ musicbrainzngs.set_useragent(
     agent_version,
     "https://github.com/km0m/Manage-song-files.git",
 )
-sys.stdout.write("Please input your Musicbrainz username: ")
-username = user_input()
-password = getpass.getpass("Enter your Musicbrainz password: ")
 
-musicbrainzngs.auth(username, password)
 musicbrainzngs.set_rate_limit(limit_or_interval=False, new_requests=1)
 
-musicbrainzngs.musicbrainz.VALID_RELEASE_STATUSES = ['official']
+result = musicbrainzngs.search_artists(query=artist_name, limit=None, offset=None, strict=False)
 
-musicbrainzngs.get_artist_by_id('Oasis', includes=['recordings', 'releases', 'discids', 'media'])
+for artist in result['artist-list']:
+    print("{name}: {id}".format(name=artist["name"], id=artist['id']))
