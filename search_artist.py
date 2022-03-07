91b93ce173a7432dec24ce1e49e1128054349cdd
Km0m <karolina.ordonez@gmail.com>
2022-03-07 18:55:16 +0100

    Adding variables & set_hostname

--- search_artist.py
+++ search_artist.py
@@ -8,10 +8,21 @@ Created on Sun Mar  6 18:42:34 2022
 import musicbrainzngs
 import sys
 
+agent_name = "password_test.py"
+agent_version = "0.1"
+# this script doesn't submit anything (empty dict)
+# so testing this on live server (with live passwords) is okay.
+musicbrainz_server = "test.musicbrainz.org"
+#musicbrainz_server = "musicbrainz.org"
+
 musicbrainzngs.set_useragent(
     "python-search-artist_example",
     "0.1",
     "https://github.com/km0m/Manage-song-files.git",
 )
+
+musicbrainzngs.set_rate_limit(limit_or_interval=1.0, new_requests=1)
+
+musicbrainzngs.musicbrainz.VALID_RELEASE_STATUSES = ['official']
 
 musicbrainzngs.get_artist_by_id('Oasis', includes=['recordings', 'releases', 'discids', 'media'])
