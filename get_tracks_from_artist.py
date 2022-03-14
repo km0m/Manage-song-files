2d54257d7f7dc1f4218266543d5f8ccc9115f3f9
Km0m <karolina.ordonez@gmail.com>
2022-03-15 00:50:47 +0100

    Modified get_tracklist to return an all_tracks dictionary

--- get_tracks_from_artist.py
+++ get_tracks_from_artist.py
@@ -19,21 +19,30 @@ musicbrainzngs.set_useragent(
 
 def get_tracklist(artist):
     artist_id = musicbrainzngs.search_artists(artist=artist, strict=True)['artist-list'][0]['id']
-    result = musicbrainzngs.browse_releases(artist=artist_id, release_status=['official'], release_type=['album'], includes=['recordings'], limit=10, offset=0)
+    result = musicbrainzngs.browse_releases(artist=artist_id, release_status=['official'], release_type=['album'], includes=['recordings'], limit=100, offset=0)
+    all_tracks = {}
     for i in range(len(result["release-list"])):
         #### get tracklist
+        track_list = []
         id = result["release-list"][i]["id"]
         new_result = musicbrainzngs.get_release_by_id(id, includes=["recordings"])
         t = (new_result["release"]["medium-list"][0]["track-list"])
+        title = new_result["release"]["title"]
+#        print(title)
         for x in range(len(t)):
             line = (t[x])
-            print(f'{line["number"]}. {line["recording"]["title"]}')
+            track_list.append(line["recording"]["title"])
+#            print(f'{line["number"]}. {line["recording"]["title"]}')
+        all_tracks[title] = track_list
+        print(all_tracks)
+    return all_tracks
+
 
 if __name__ == '__main__':
     ### get first release
     artist = input("Artist: ")
     # album = input("Album: ")
     if not artist == "":
-        get_tracklist(artist)
+        print(get_tracklist(artist))
     else:
         print("Artist or Album missing")
